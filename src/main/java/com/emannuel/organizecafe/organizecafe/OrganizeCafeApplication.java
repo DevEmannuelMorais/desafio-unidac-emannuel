bfd6ffc4dfffd9e4908d725030b7f6bcf0f0fd40
Emannuel Morais <emanuelmorais2015@gmail.com>
2023-07-23 10:42:53 -0300

    back

--- src/main/java/com/emannuel/organizecafe/organizecafe/OrganizeCafeApplication.java
+++ src/main/java/com/emannuel/organizecafe/organizecafe/OrganizeCafeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrganizeCafeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrganizeCafeApplication.class, args);
 	}
